8e36a0db39e8f38c3a9b38ec69347be02d62ad45
Vanderkump <98603917+Vanderkump@users.noreply.github.com>
2024-08-20 13:51:41 +0000

    teste

---
+++ Basic Python/variaveis.py
@@ -0,0 +1,10 @@
+nome = "Felipe"
+profissão = "ECA"
+cargo = "projetista"
+idade = "18"
+gostar_de = "RPG"
+
+print(f"Meu nome é {nome} e tenho {idade}")
+print(f"trabalho como {profissão}")
+print(f"atuo como {cargo}")
+print(f"meu hobby é {gostar_de}")
