60bd5ee912577d6ebc9df2aca51f557701df0149
Vanderkump <98603917+Vanderkump@users.noreply.github.com>
2024-08-20 14:09:55 +0000

    teste

---
+++ Basic Python/operadores.py
@@ -0,0 +1,28 @@
+num1 = 5 #int
+num2 = 3.5 #float
+
+print(type(num1)) #mostrar o tipo de variavel
+
+'''
+a = float(5) #conversão de tipo de variavel de int para float
+print(a)
+print(type(a))
+
+b = int(num2) #conversão de tipo de variavel de float para int
+print(b)
+print(type(b))
+'''
+'''
+a = "5"
+print(type(a))
+a = int(a)
+print(type(a))
+'''
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+print(num1 ** num2)
+print(num1 // num2)
+
--- Basic Python/variaveis.py
+++ Basic Python/variaveis.py
@@ -1,8 +1,10 @@
-nome = "Felipe"
-profissão = "ECA"
+nome = "Felipe" #string
+profissão = "ECA" 
 cargo = "projetista"
-idade = "18"
 gostar_de = "RPG"
+idade = 23 #int
+salario = 20.40 #float
+is_male = true #booleana recebe true ou false
 
 print(f"Meu nome é {nome} e tenho {idade}")
 print(f"trabalho como {profissão}")
