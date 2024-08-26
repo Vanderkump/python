21c63031449169ba36896a700c7018ce4c4ed11c
Vanderkump <98603917+Vanderkump@users.noreply.github.com>
2024-08-26 14:08:35 +0000

    teste

--- POO/__pycache__/aluno.py
+++ POO/__pycache__/aluno.py
@@ -1,7 +1,22 @@
 class Aluno:
-    def __init__(self, matricula,nome,curso,disciplina):
+    def __init__(self, matricula,nome,curso,disciplinas, disciplina_disponiveis):
         self.matricula = matricula
         self.nome = nome
         self.curso = curso
-        self.disciplina = disciplina  
-    
+        self.disciplinas = []
+
+        for disc in disciplinas:
+            for disc_disp in disciplina_disponiveis:
+                if disc == disc_disp.nome:
+                    self.disciplinas = disciplinas.append(disc)
+                    print("Aluno {} foi matriculado na disciplina {}". format(self.nome, disc))
+                    break
+                else:
+                    print("Aluno {} não foi matriculado na disciplina {}". format(self.nome, disc))
+
+    def insere_disciplina(self, disciplina):
+        if disciplina in disciplinas_disponiveis:
+            self.disciplinas.append(disciplina)
+            return "Aluno foi matriculado na disciuplina{}".format(disciplina)
+        else:
+            return "Aluno não foi matriculado na disciuplina{}".format(disciplina)
--- POO/__pycache__/main.py
+++ POO/__pycache__/main.py
@@ -1,3 +1,22 @@
 from aluno import Aluno
+from disciplina import Disciplina
 
-aluno = Aluno("22016733730", "Felipe", "ECA", ["POO", "tecnicas de programação", "Eletronica"])
+disciplina_disponiveis = []
+
+disc = Disciplina("POO", "Alyson", 64, "LEC1")
+disciplina_disponiveis.append(disc)
+
+disc = Disciplina("Matemática", "Roberto Carlos", 32, "I2118")
+disciplina_disponiveis.append(disc)
+
+aluno = Aluno("22016733730", "Felipe", "ECA", ["POO", "tecnicas de programação", "Eletronica", disciplina_disponiveis])
+aluno1 = Aluno("09354687390", "julia", "ECA", ["POO", "tecnicas de programação", disciplina_disponiveis])
+
+print(aluno.disciplinas)
+print(aluno1.disciplinas)
+
+print(aluno.insere_disciplina("Ciências Humanas"))
+    
+
+print(aluno.disciplinas)
+print(aluno1.disciplinas)
