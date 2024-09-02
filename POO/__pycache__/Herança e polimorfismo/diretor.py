03adfab09eb22b4db6c825fcf22a6ad5d7beae19
Vanderkump <98603917+Vanderkump@users.noreply.github.com>
2024-09-02 14:11:43 +0000

    teste

--- POO/__pycache__/Herança e polimorfismo/diretor.py
+++ POO/__pycache__/Herança e polimorfismo/diretor.py
@@ -3,7 +3,6 @@ from pessoa import Pessoa
 class Diretor(Pessoa):
     def __init__(self, cpf, siape, nome, curso, disciplinas,
     disciplinas_disponiveis, instituto, salario):
-        super().__init__(nome, cpf, curso, disciplinas,
-        disciplinas_disponiveis, salario)
+        super().__init__(nome, cpf, curso, disciplinas, disciplinas_disponiveis, salario)
         self.siape = siape
         self.instituto = instituto
