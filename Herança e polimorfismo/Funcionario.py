9e4e60b9fdcf1ff34f60958053d9f3c6f8267ac2
Vanderkump <98603917+Vanderkump@users.noreply.github.com>
2024-09-02 13:43:21 +0000

    teste

---
+++ Herança e polimorfismo/Funcionario.py
@@ -0,0 +1,5 @@
+class Funcionario():
+    def __init__(self, nome, cpf, salario):
+        self._nome = nome
+        self._cpf = cpf
+        self._salario = salario
---
+++ Herança e polimorfismo/Gerente.py
@@ -0,0 +1,10 @@
+from Funcionario import Funcionario
+
+class Gerente(Funcionario):
+    def __init__(self, nome, cpf, salario, senha,login, qtd_gerenciados):
+        super().__init__(nome, cpf, salario)
+        self.login = login
+        self._senha = senha
+        self._qtd_gerenciados = qtd_gerenciados
+
+gerente = Gerente("Neymar", "22016787656", 50000, "senha", "neyfilho", 10)
