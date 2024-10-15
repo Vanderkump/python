e96ed2e8c7480d7850d6aa1d2ec689b5192936c7
Vanderkump <98603917+Vanderkump@users.noreply.github.com>
2024-10-15 01:14:51 +0000

    teste

--- estrutura de dados/lista encadeada.py
+++ estrutura de dados/lista encadeada.py
@@ -10,34 +10,40 @@ class Lista():
     def insere(self, num):
         novo_no = No(num)
 
+        # Se a lista estiver vazia, insira o novo nó como cabeça
         if self.cabeca == None:
             self.cabeca = novo_no
+            return
 
-        aux = self.cabeca
-        while(True):
-            if aux == None or aux.num > num:
-                break  
-            aux = aux.prox
-
-        if aux == self.cabeca: #primeiro elemento da lista
+        # Se o novo elemento for menor que o primeiro elemento (cabeça), insira no início
+        if num < self.cabeca.num:
             novo_no.prox = self.cabeca
-            self.cabeca = novo_no 
-        #elif aux = None: #ultimo elemento
-        #else: #elemento do meio 
+            self.cabeca = novo_no
+            return
+
+        # Caso contrário, encontrar o local correto de inserção (meio ou fim)
+        aux = self.cabeca
+        while aux.prox is not None and aux.prox.num < num:
+            aux = aux.prox
+        
+        # Agora insere o novo nó no local apropriado
+        novo_no.prox = aux.prox
+        aux.prox = novo_no
 
     def imprime(self):
         aux = self.cabeca
-        while(True):
+        while aux is not None:
             print(aux.num)
             aux = aux.prox
 
-            if aux == None:
-                break
-
+# Testando a lógica
 lista = Lista()
 lista.insere(5)
 lista.insere(4)
 lista.insere(3)
 lista.insere(2)
 lista.insere(1)
+lista.insere(6)  # Inserindo no final
+lista.insere(0)  # Inserindo no início
+lista.insere(4.5) # Inserindo no meio
 lista.imprime()
